b0c41b941566d670007461639029e38d52292512
Bloxxel64 <70494679+Bloxxel64@users.noreply.github.com>
2023-03-29 19:23:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+import os
+
+splashtexts = ["It's a sad day to be a Goblin.", "Don't you have better things to do?", "Enjoy your stay.", "Now how did you end up here of all places?"]
+print("Welcome to The Dungeon")
+print(splashtexts[random.randint(0, 3)])
